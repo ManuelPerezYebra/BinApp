c0cdc2a65e1330c5518549ed0c00c64bd45ee160
Manuel <manuelperezyebra@gmail.com>
2024-11-05 19:22:28 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,19 @@ const Header = () => {
 		<StyledHeader>
 			<h3>
 				MAD7 <strong>Bin Pick Cons Filter</strong>
-			</h1>
+			</h3>
+			<DevelopText>
+				<p>
+					<strong>
+						Está aplicación ha sido desarrollada por @Yebram, envía un slack o
+						un chime con tu feedback. Gracias
+					</strong>
+				</p>
+				<p>
+					This app was developed for @yebram, send slack or chime with your
+					feedback. Thank you.
+				</p>
+			</DevelopText>
 		</StyledHeader>
 	);
 };
